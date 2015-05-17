a5d42e7714391a328fcca71981ab689e864fcd42
eepoch <eepoch@163.com>
2015-05-17 14:11:10 +0800

    Long == 比较的trap

---
+++ src/org/hq/detail/LongTrap.java
@@ -0,0 +1,31 @@
+package org.hq.detail;
+
+public class LongTrap {
+
+	/**
+	 * 丧心病狂的包装类型细节:Long 和Integer 缓存了 [-128, 127]范围内的值。
+	 * This is the result of auto-boxing. See Long.valueOf().
+	 */
+	public static void main(final String[] args) {
+		final Long n = 0L;
+		final Long m = 0L;
+		System.out.println(n + " == " + m + " : " + (n == m));
+
+		final Long a = 127L;
+		final Long b = 127L;
+		System.out.println(a + " == " + b + " : " + (a == b));
+
+		final Long A = 128L;
+		final Long B = 128L;
+		System.out.println(A + " == " + B + " : " + (A == B));
+
+		final Long x = -128L;
+		final Long y = -128L;
+		System.out.println(x + " == " + y + " : " + (x == y));
+
+		final Long X = -129L;
+		final Long Y = -129L;
+		System.out.println(X + " == " + Y + " : " + (X == Y));
+	}
+
+}
