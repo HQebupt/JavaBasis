89f104a69f37742f30d9c2ba4fe031dab43dc54e
HQ <qiang.huang1991@gmail.com>
2015-08-25 16:26:19 +0800

    自己实现ArrayList

---
+++ src/org/hq/java/lession3/EBArray.java
@@ -0,0 +1,234 @@
+package org.hq.java.lession3;
+
+import java.util.AbstractCollection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @version 1.0
+ * @author HuangQiang
+ * @date 2013-10-26
+ * @update 2015-8-24
+ */
+
+public class EBArray extends AbstractCollection<Object> {
+	private int[] elementData;
+	private int size; // elementData数组已经使用的大小
+	private static final int MAX_ARRAY_SIZE = 500;
+
+	public EBArray() {
+		this(MAX_ARRAY_SIZE);
+	}
+
+	/**
+	 * @param initialCapacity
+	 */
+	public EBArray(int initialCapacity) {
+		if (initialCapacity < 0 || initialCapacity > MAX_ARRAY_SIZE)
+			throw new IllegalArgumentException(
+					"Illegal Capacity(Capacity <0 or more than MAX_ARRAY_SIZE 500): "
+							+ initialCapacity);
+		this.elementData = new int[initialCapacity];
+	}
+
+	/**
+	 * @param index
+	 * @return 返回index下标的元素
+	 */
+	public int get(int index) {
+		rangeCheck(index);
+		return elementData[index];
+	}
+
+	/**
+	 * @param index
+	 */
+	private void rangeCheck(int index) {
+		if (index >= size)
+			throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+	}
+
+	/**
+	 * @param index
+	 * @return error message
+	 */
+	private String outOfBoundsMsg(int index) {
+		return "Index: " + index + ", Size: " + size;
+	}
+
+	/*
+	 * 获取当前数组内的元素个数。
+	 */
+	public int size() {
+		return size;
+	}
+
+	/**
+	 * @param num
+	 * @return 是否添加成功
+	 */
+	public boolean add(int num) {
+		if (ensureCapacityEnough(size + 1)) {
+			elementData[size++] = num;
+			return true;
+		} else {
+			System.out.println("EBArray's data has reached  maxCapacity : "
+					+ elementData.length + ".\n" + "add number: " + num
+					+ " failed!\n");
+			return false;
+		}
+	}
+
+	/**
+	 * 确保容量够不够用
+	 */
+	private boolean ensureCapacityEnough(int size) {
+		if (size <= elementData.length)
+			return true;
+		else
+			return false;
+
+	}
+
+	/**
+	 * 移除EBArray中第一个值为num的元素
+	 * 
+	 * @param num
+	 * @return
+	 */
+	public boolean remove(int num) {
+		for (int index = 0; index < size; index++)
+			if (elementData[index] == num) {
+				fastRemove(index);
+				return true;
+			}
+		return false;
+	}
+
+	/**
+	 * 移除EBArray中所有值为num的元素
+	 * 
+	 * @param num
+	 * @return
+	 */
+	public boolean removeAll(int num) {
+		int sizebefore = size;
+		for (int index = 0; index < size; index++)
+			if (elementData[index] == num) {
+				fastRemove(index);
+			}
+		if (sizebefore > size)
+			return true;
+		else
+			return false;
+	}
+
+	/**
+	 * @param index
+	 */
+	private void fastRemove(int index) {
+		int numMoved = size - index - 1;
+		if (numMoved > 0)
+			System.arraycopy(elementData, index + 1, elementData, index,
+					numMoved);
+		size--; // 隐患：没有清除掉数组边界的值。改进方法：全部使用Object[] elementData,
+				// 用到elementData的方法要强制转换为Integer。
+	}
+
+	/**
+	 * @param num
+	 * @return
+	 */
+	public boolean contains(int num) {
+		return indexOf(num) >= 0;
+	}
+
+	/**
+	 * @param num
+	 * @return 返回找到元素的下标
+	 */
+	public int indexOf(int num) {
+		for (int i = 0; i < size; i++)
+			if (elementData[i] == num)
+				return i;
+		return -1;
+	}
+
+	/**
+	 * 打印数组
+	 */
+	public static void print(EBArray ebarray) {
+		int ebsize = ebarray.size();
+		System.out.println("当前数组的容量是: " + ebsize + ".\n" + "数组元素如下：");
+		if (ebsize == 0) {
+			System.out.println("null.");
+			return;
+		}
+		Iterator<Integer> it = ebarray.iterator();
+		int count = 0;
+		boolean count20 = false;
+		while (it.hasNext()) {
+			System.out.print(it.next() + " ; ");
+			count++;
+			if (count % 20 == 0) {
+				count20 = true;
+				System.out.println();
+				count20 = false;
+			}
+		}
+		System.out.println();
+	}
+
+	@Override
+	public Iterator iterator() {
+		return new Itr();
+	}
+
+	/**
+	 * Iterator的实现
+	 */
+	private class Itr implements Iterator<Integer> {
+		int cursor = 0; // index of next element to return
+		int lastRet = -1; // index of last element returned; -1 if no such
+
+		public boolean hasNext() {
+			return cursor != EBArray.this.size();
+		}
+
+		public Integer next() {
+			try {
+				int i = cursor;
+				int next = EBArray.this.get(i);
+				lastRet = i;
+				cursor = i + 1;
+				return Integer.valueOf(next);
+			} catch (IndexOutOfBoundsException e) {
+				throw new NoSuchElementException();
+			}
+		}
+
+		/**
+		 * 移除当前的元素
+		 */
+		public void remove() {
+			if (lastRet < 0)
+				throw new IllegalStateException();
+			try {
+				EBArray.this.fastRemove(lastRet);
+				cursor = lastRet;
+				lastRet = -1;
+			} catch (IndexOutOfBoundsException ex) {
+				throw new ConcurrentModificationException();
+			}
+		}
+	}
+
+	public static void testIteratorRemove(EBArray ebarray) {
+		Iterator it = ebarray.iterator();
+		while (it.hasNext()) {
+			it.next();
+			it.remove();
+		}
+	}
+}
---
+++ src/org/hq/java/lession3/Main.java
@@ -0,0 +1,68 @@
+package org.hq.java.lession3;
+
+import java.util.Random;
+
+/**
+ * 
+ * @author HuangQiang
+ * @date 2013-10-27
+ */
+public class Main {
+	private static final int NUMMAX = 100;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		welcome();
+		test();
+	}
+
+	public static void welcome() {
+		System.out.println("Welcome to Homework 3 from Group 3!\n"
+				+ "Our members are 王岩 单劼 黄强 刘化东 张宇飞.\n");
+		System.out.println("下面是EBArray类的方法演示：\n" + "1.EBArray类创建。\n"
+				+ "2.EBArray方法的三个方法：add、remove、contains。\n"
+				+ "3.Iterator三个方法的使用：hasNext、next、remove。");
+	}
+
+	public static void test() {
+		Random rand = new Random(System.currentTimeMillis());
+		System.out
+				.println("\n\n****************************这是一个随机测试用例。****************************");
+		System.out.println("新建EBarray实例，初始化最大容量为500；随机添加500个int元素(0~300之间).\n");
+		EBArray ebarray = new EBArray();
+		for (int i = 1; i <= 500; i++)
+			ebarray.add(rand.nextInt(NUMMAX));
+		System.out.println("1.测试EBArray add方法,添加超过数组容量的数据，打印报错信息！！");
+		ebarray.add(rand.nextInt(NUMMAX));
+		EBArray.print(ebarray);
+
+		int numcontain = rand.nextInt(NUMMAX);
+		System.out.println("2.测试EBArray contains方法,测试数组中是否有number: "
+				+ numcontain + " ?");
+		System.out.println("contained ? :" + ebarray.contains(numcontain)
+				+ "\n");
+
+		int numremove = rand.nextInt(NUMMAX);
+		System.out
+				.println("3.测试EBArray remove方法,随机移除一个随机值:" + numremove + " ?");
+		System.out.println("remove " + numremove + " successful ? :"
+				+ ebarray.remove(numremove));
+		EBArray.print(ebarray);
+
+		int numremoveAll = rand.nextInt(NUMMAX);
+		System.out.println("\n4.测试EBArray removeAll方法,随机移除一个随机值:"
+				+ numremoveAll + "的所有元素 ?");
+		System.out.println("removeAll " + numremoveAll + " successful ? :"
+				+ ebarray.removeAll(numremoveAll));
+		EBArray.print(ebarray);
+
+		System.out.println("\n5.测试Iterator remove方法，移除所有数组元素：");
+		EBArray.testIteratorRemove(ebarray);
+		EBArray.print(ebarray);
+
+		System.out
+				.println("\n\n************************************测试完毕！************************************");
+	}
+}
